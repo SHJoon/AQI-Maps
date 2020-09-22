04fc598a59b4216455ea0a1ba4dee3b440f22fa1
SHJoon <52848941+SHJoon@users.noreply.github.com>
2020-09-22 10:12:17 -0700

    Playing around with infobox styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Map} from './components/maps';
 
 
 function App({ mapProps }) {
-  const [AQIStations1, setAQIStations] = useState();
+  const [AQIStations, setAQIStations] = useState([]);
 
   useEffect(() => {
     axios.get("https://api.waqi.info/map/bounds/?token=d2583b4394214a830ffdade2d10b103620d66ee7&latlng=24.846565,-65.960261,48.987427,-124.732715")
@@ -16,59 +16,8 @@ function App({ mapProps }) {
     })
     .catch((error) => console.log(error));
   }, [])
-  console.log("AQIStations1:",AQIStations1);
-  var AQIStations= [
-    {
-    lat: 44.657014,
-    lon: -111.089618,
-    uid: 7529,
-    aqi: "21",
-    station: {
-    name: "West Yellowstone Park Ent #2, Montana, USA",
-    time: "2020-09-21T19:00:00-07:00"
-    }
-    },
-    {
-    lat: 40.733501,
-    lon: -111.871696,
-    uid: 4469,
-    aqi: "68",
-    station: {
-    name: "Salt Lake City, Utah",
-    time: "2020-09-21T20:00:00-06:00"
-    }
-    },
-    {
-    lat: 44.150528,
-    lon: -77.3955,
-    uid: 1,
-    aqi: "25",
-    station: {
-    name: "Belleville, Ontario, Canada",
-    time: "2020-09-21T22:00:00-05:00"
-    }
-    },
-    {
-    lat: 37.1314,
-    lon: -86.1481,
-    uid: 7411,
-    aqi: "22",
-    station: {
-    name: "Mammoth Cave NP, Kentucky, USA",
-    time: "2020-09-21T20:00:00-05:00"
-    }
-    },
-    {
-    lat: 39.758235,
-    lon: -84.197375,
-    uid: 7463,
-    aqi: "29",
-    station: {
-    name: "Dayton - Sinclair, Ohio, USA",
-    time: "2020-09-21T20:00:00-05:00"
-    }
-    }]
-
+  console.log("AQIStations:",AQIStations);
+  
   const addMarkers = links => map => {
     links.forEach((link, index) => {
       var position = new window.google.maps.LatLng(link.lat, link.lon);
@@ -79,8 +28,16 @@ function App({ mapProps }) {
         title: link.station.name,
         id: index + 1
       });
+
+      const infoStr = 
+      `<div>
+        <h4>${link.station.name}</h4>
+        <p>AQI: ${link.aqi}</p>
+        <p>Last Updated: ${link.station.time}</p>
+      </div>`
+
       const infowindow = new window.google.maps.InfoWindow({
-        content: link.name
+        content: infoStr
       });
       marker.addListener(`click`, () => {
         infowindow.open(map, marker);
@@ -123,10 +80,6 @@ function App({ mapProps }) {
   );
 }
 
-<<<<<<< HEAD
 export default App;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-=======
-export default App;
->>>>>>> Allowing App export
